dfde5b79d31903542d2cc4eab9b5f7f83e6066a3
janiprog001 <147839929+janiprog001@users.noreply.github.com>
2025-02-07 13:35:40 +0100

    appsettings.json & Programcs

--- ParkingGarageApi/Program.cs
+++ ParkingGarageApi/Program.cs
@@ -1,15 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using ParkingGarageApi.Data;
+using ParkingGarageApi.Services;
+using ParkingGarageApi.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Konfiguráld a MySQL adatbázis kapcsolatot
+// A kapcsolati stringet a appsettings.json fájlban kell definiálni (példa: "DefaultConnection")
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 33)) // Állítsd be a megfelelõ MySQL verziót!
+    ));
 
+// Regisztráljuk a controller-eket
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Swagger (OpenAPI) dokumentáció engedélyezése
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Regisztráljuk a saját szolgáltatásainkat
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IParkingService, ParkingService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Fejlesztõi környezetben engedélyezhetjük a Swagger UI-t
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -18,6 +35,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Itt jöhet az autentikációs middleware (pl. JWT) ha szükséges
 app.UseAuthorization();
 
 app.MapControllers();
