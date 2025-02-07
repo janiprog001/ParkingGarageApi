f2ceab9c3fa6cc8b4fdda2fe74d86d614b91ffa8
janiprog001 <147839929+janiprog001@users.noreply.github.com>
2025-02-07 13:27:41 +0100

    Services\Interfaces

---
+++ ParkingGarageApi/Sevices/Interfaces/IParkingService.cs
@@ -0,0 +1,14 @@
+﻿using ParkingGarageApi.Models;
+
+namespace ParkolohazAPI.Services.Interfaces
+{
+    /// <summary>
+    /// A parkolóhely kezelést definiáló szolgáltatás interfésze.
+    /// </summary>
+    public interface IParkingService
+    {
+        Task<IEnumerable<ParkingSpot>> GetAllSpotsAsync();
+        Task<ParkingSpot> UpdateSpotStatusAsync(int spotNumber, string action);
+        Task<(int free, int occupied)> GetParkingSummaryAsync();
+    }
+}
---
+++ ParkingGarageApi/Sevices/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using ParkingGarageApi.Models;
+using ParkingGarageApi.DTOs;
+
+namespace ParkolohazAPI.Services.Interfaces
+{
+    /// <summary>
+    /// A felhasználói műveleteket definiáló szolgáltatás interfésze.
+    /// </summary>
+    public interface IUserService
+    {
+        Task<User> RegisterAsync(RegisterRequest request);
+        Task<User> LoginAsync(LoginRequest request);
+    }
+}
